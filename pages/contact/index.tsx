67bb9734f9ad6ff6807057c4c5fe4e3a213f7bcd
Wassini Bouzidi <wassini.bouzidi@epitech.eu>
2025-05-19 19:44:06 +0200

    feat: update button styling in Contact component for improved hover effect

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -61,7 +61,8 @@ export default function Contact() {
                   href="https://www.google.com/maps/place/81+rue+Paul+Vaillant+Couturier,+92230+Gennevilliers"
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="inline-flex items-center bg-brand hover:bg-purple-700 text-white font-semibold px-4 py-2 rounded-lg transition"
+                  className="inline-flex items-center bg-brand hover:bg-brand-dark
+                   text-white font-semibold px-4 py-2 rounded-lg transition"
                 >
                   <MapPin className="w-5 h-5 mr-2" />
                   Ouvrir la carte
